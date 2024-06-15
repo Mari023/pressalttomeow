2ad0c3c0c5dc217d0301e74cdc43a2237f72a618
slonkazoid <slonkazoid@slonk.ing>
2024-06-15 16:41:50 +0300

    port to 1.21

--- src/main/java/de/mari_023/pressalttomeow/MeowPayload.java
+++ src/main/java/de/mari_023/pressalttomeow/MeowPayload.java
@@ -7,7 +7,7 @@ import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
 public class MeowPayload implements CustomPacketPayload {
-    public static Type<MeowPayload> TYPE = new Type<>(new ResourceLocation("pressalttomeow", "m"));
+    public static Type<MeowPayload> TYPE = new Type<>(ResourceLocation.tryBuild("pressalttomeow", "m"));
     public static StreamCodec<ByteBuf, MeowPayload> CODEC = StreamCodec.of((buffer, value) -> {
     }, (buffer) -> new MeowPayload());
 
